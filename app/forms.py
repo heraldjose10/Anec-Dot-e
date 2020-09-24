f02909565d0aba9509d987690697ce4d7fee4041
unknown <heraldjose10@gmail.com>
2020-09-24 13:24:50 +0530

    added registration

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,ValidationError,Email,EqualTo
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep Me Signed In')
     submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired()])
+    email = StringField('Email-ID',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password',validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign In')
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username Exists')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('This E-mail ID is already registered')
+       
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from app import app
-from app.forms import LoginForm
-from flask import render_template, url_for, redirect, flash
-from flask_login import current_user, login_user, logout_user
+from app import db
+from app.forms import LoginForm, RegistrationForm
+from flask import render_template, url_for, redirect, flash, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
 
     posts = [
@@ -41,7 +44,10 @@ def login():
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
 
     return render_template('loginpage.html', title='Sign In', form=form)
 
@@ -50,3 +56,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Congratulations.. You are Registered..!!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)

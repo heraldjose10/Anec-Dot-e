591f78d71da7ce7ecef847ba5a912ccccb239e55
unknown <heraldjose10@gmail.com>
2020-10-05 22:54:48 +0530

    added about_me form and 400,500 errors

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app import db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask import render_template, url_for, redirect, flash, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
@@ -102,3 +102,19 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen= datetime.utcnow()
         db.session.commit()
+
+
+@app.route('/edit_profile',methods=['GET','POST'])
+@login_required
+def edit_profile():
+    form=EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your Changes Has Been Saved')
+        return(redirect(url_for('edit_profile')))
+    elif request.method=='GET':
+        form.username.data=current_user.username
+        form.about_me.data=current_user.about_me
+    return render_template('edit_profile.html',title='Edit_Profile',form=form)

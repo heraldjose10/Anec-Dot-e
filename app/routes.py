cbd64c2efe66e0f9d1dda89add3919e88188d029
unknown <heraldjose10@gmail.com>
2020-10-08 12:53:14 +0530

    added follow functionality

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app import db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from flask import render_template, url_for, redirect, flash, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
@@ -81,6 +81,7 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
+    form = EmptyForm()
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
         {
@@ -94,27 +95,75 @@ def user(username):
             'link': 'http://charlesleifer.com/blog/saturday-morning-hack-a-little-note-taking-app-with-flask/'
         }
     ]
-    return render_template('user.html',title='Profile',user=user,posts=posts)
+    return render_template('user.html', title='Profile', user=user, posts=posts,form=form)
 
 
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
-        current_user.last_seen= datetime.utcnow()
+        current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
 
-@app.route('/edit_profile',methods=['GET','POST'])
+@app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form=EditProfileForm(current_user.username)
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your Changes Has Been Saved')
         return(redirect(url_for('edit_profile')))
-    elif request.method=='GET':
-        form.username.data=current_user.username
-        form.about_me.data=current_user.about_me
-    return render_template('edit_profile.html',title='Edit_Profile',form=form)
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit_Profile', form=form)
+
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+
+        if user is None:
+            flash('No user named {}'.format(username))
+            return redirect(url_for('index'))
+
+        if user == current_user:
+            flash('You cannot Follow Yourself')
+            return redirect(url_for('user', username=username))
+
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are now following {}'.format(username))
+        return redirect(url_for('user', username=username))
+
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+
+        if user is None:
+            flash('No user named {} Exists'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow You')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash("you unfollowed {}".format(username))
+        return redirect(url_for('user', username=username))
+
+    else:
+        return redirect(url_for('index'))
